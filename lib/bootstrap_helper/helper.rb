3fa5e4ac07817474d8dbf707fcaa962894741b2a
Albert Song <albb0920@gmail.com>
2012-08-01 17:10:02 +0800

    Drop all helpers that isn't helping bootstrap.

--- lib/bootstrap_helper/helper.rb
+++ lib/bootstrap_helper/helper.rb
@@ -1,32 +1,10 @@
 # coding: utf-8
 module BootstrapHelper
-  
   module Helper
     def yield_or_default(message, default_message = "")
       message.nil? ? default_message : message
     end
 
-
-    def render_page_title
-      title = @page_title ? "#{SITE_NAME} | #{@page_title}" : SITE_NAME rescue "SITE_NAME"
-      content_tag("title", title, nil, false)
-    end
-
-    def render_body_tag
-      class_attribute = ["#{controller_name}-controller","#{action_name}-action"].join(" ")
-      id_attribute = (@body_id)? " id=\"#{@body_id}-page\"" : ""
-
-      raw(%Q|<!--[if lt IE 7 ]>
-<body class="#{class_attribute} ie6"><![endif]-->
-<!--[if gte IE 7 ]>
-<body class="#{class_attribute} ie"><![endif]-->
-<!--[if !IE]>-->
-<body#{id_attribute} class="#{class_attribute}">
-<!--<![endif]-->|)
-
-    end
-    
-    
     def notice_message
       flash_messages = []
       flash.each do |type, message|
@@ -35,11 +13,6 @@ module BootstrapHelper
         flash_messages << text if message
       end
       flash_messages.join("\n").html_safe
-    end
-
-
-    def s(html)
-      sanitize( html, :tags => %w(table thead tbody tr td th ol ul li div span font img sup sub br hr a pre p h1 h2 h3 h4 h5 h6), :attributes => %w(id class style src href size color) )
     end
 
     def render_table(rows, renderrers, table_options = {})
